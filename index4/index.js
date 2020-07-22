cfdd77c8b28c960aa9832cba66fad4d9ec97ff5f
annaHsu <sstsuki@gmail.com>
2020-07-23 01:18:43 +0800

    第四周
    分頁元件

--- index4/index.js
+++ index4/index.js
@@ -5,7 +5,7 @@ new Vue({
   data:
   {
     myProducts:'',
-    myPages:'',
+    myPagination:'',
     currProduct:{
       imgUrl:[],
     },
@@ -23,7 +23,8 @@ new Vue({
     //主要用於取得 token 若沒有使用者沒有 token 則返回到登入畫面，若有則執行「取得全部產品」的方法。   
     this.user.token = getCookie("token");
     this.user.uuid = getCookie("annauuid");
-        
+    
+    
     if(this.user.token === '' || this.user.uuid === '')
       window.location("login.html");
 
@@ -32,12 +33,13 @@ new Vue({
   },
   methods:{
     getProducts(page = 1){
+      //console.log("getList="+page);
       const url = `${apiUrl}/${this.user.uuid}/admin/ec/products?page=${page}`;
       //預設帶入 token
       axios.defaults.headers.common.Authorization = `Bearer ${this.user.token}`;
       axios.get(url).then((resp)=>{
         this.myProducts = resp.data.data;
-        this.myPages = resp.data.meta.pagination;
+        this.myPagination = resp.data.meta.pagination;
       })
 
     },
--- index4/pagination.js
+++ index4/pagination.js
@@ -1,25 +1,25 @@
-Vue.component('publicpages', {
+Vue.component('pagination', {
     // 分頁模板
     template: `<nav aria-label="Page navigation example">
     <ul class="pagination">
       <li
         class="page-item"
-        :class="{'disabled': pages.current_page === 1}"
+        :class="{'disabled': myPages.current_page === 1}"
       >
         <a
           class="page-link"
           href="#"
           aria-label="Previous"
-          @click.prevent="emitPages(pages.current_page - 1)"
+          @click.prevent="emitPages(myPages.current_page - 1)"
         >
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
       <li
-        v-for="(item, index) in pages.total_pages"
+        v-for="(item, index) in myPages.total_pages"
         :key="index"
         class="page-item"
-        :class="{'active': item === pages.current_page}"
+        :class="{'active': item === myPages.current_page}"
       >
         <a
           class="page-link"
@@ -29,13 +29,13 @@ Vue.component('publicpages', {
       </li>
       <li
         class="page-item"
-        :class="{'disabled': pages.current_page === pages.total_pages}"
+        :class="{'disabled': myPages.current_page === myPages.total_pages}"
       >
         <a
           class="page-link"
           href="#"
           aria-label="Next"
-          @click.prevent="emitPages(pages.current_page + 1)"
+          @click.prevent="emitPages(myPages.current_page + 1)"
         >
           <span aria-hidden="true">&raquo;</span>
         </a>
@@ -54,16 +54,16 @@ Vue.component('publicpages', {
      * 主要接受由外(Products)向內(pagination)傳遞的分頁物件，意指在 getProducts 取得的分頁物件
      */
     props: {
-      pages: {},
+      myPages: {},
     },
     methods: {
       /**
        * emitPage
        * @param item 是你點擊的分頁按鈕，當你點第二頁就會傳入二，點第五頁就會傳入五以此類推
        */
-      emitpages(item) {
-        // 透過 emit 向外傳遞我們點的分頁並觸發外層的 getProducts
-        this.$emit('emitpages', item);
+      emitPages(item) {
+        // 透過 emit 向外傳遞我們點的分頁並觸發外層的 getProducts        
+        this.$emit('emit-pages', item);
       },
     },
   });
